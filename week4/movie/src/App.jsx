6ac94e589856e5c6014b6e7b0bc918eb6c0f0af1
fuzzyter <165622596+fuzzyter@users.noreply.github.com>
2024-05-09 04:53:46 +0900

    :tada: week4

---
+++ week4/movie/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Spinner from './components/Spinner';
+
+import MainPage from './components/pages/MainPage';
+import PopularPage from './components/pages/PopularPage';
+import NowPlayingPage from './components/pages/NowPlayingPage';
+import TopRatedPage from './components/pages/TopRatedPage';
+import UpComing from './components/pages/UpComingPage';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+          <Route path="/popular" element={<PopularPage/>} />
+          <Route path="/now" element={<NowPlayingPage/>} />
+          <Route path="/top" element={<TopRatedPage/>} />
+          <Route path="/up" element={<UpComing/>} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
+  )
+}
+
+export default App;
