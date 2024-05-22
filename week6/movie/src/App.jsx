2ef265f288b61eca2d28649a7c00b0eeccf4bcac
fuzzyter <165622596+fuzzyter@users.noreply.github.com>
2024-05-22 22:03:03 +0900

    :tada: week6

---
+++ week6/movie/src/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Spinner from './components/Spinner';
+
+import MainPage from './components/pages/MainPage';
+import PopularPage from './components/pages/PopularPage';
+import NowPlayingPage from './components/pages/NowPlayingPage';
+import TopRatedPage from './components/pages/TopRatedPage';
+import UpComingPage from './components/pages/UpComingPage';
+import NotFoundPage from './components/pages/NotFoundPage';
+import MovieDetailPage from './components/pages/MovieDetailPage';
+import useMovies from './components/api/useMovies'; 
+import SignUpPage from './components/pages/SignUpPage';
+import LogInPage from './components/pages/LogInPage'
+import './App.css';
+
+
+function App() {
+  const { movies } = useMovies('https://api.themoviedb.org/3/movie/popular?api_key=42b8be23d71ac7e304fe02f1f4e720da&language=en-US&page=1');
+
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+          <Route path="/popular" element={<PopularPage/>} />
+          <Route path="/nowplaying" element={<NowPlayingPage/>} />
+          <Route path="/toprated" element={<TopRatedPage/>} />
+          <Route path="/upcoming" element={<UpComingPage/>} />
+          <Route path="/movie/:movieTitle" element={<MovieDetailPage movies={movies} />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LogInPage />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
+  )
+}
+
+export default App;
